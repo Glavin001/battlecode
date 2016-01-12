2a5c611891293d30f43e9df1b693d19f86163fe6
Glavin Wiechert <glavin.wiechert@gmail.com>
2016-01-12 16:33:21 -0400

    Add in Cameron's code and format properly
    Format:
    - UNIX line endings
    - Spaces not tabs

--- src/shazbot/SoldierClass.java
+++ src/shazbot/SoldierClass.java
@@ -8,42 +8,42 @@ import shazbot.RobotPlayer.Sensing;
 
 public class SoldierClass extends RobotPlayer {
 
-  public static void run() {
-    while (true) {
-      try {
-        Debug.emptyIndicatorStrings();
-        Sensing.updateNearbyEnemies();
-        Messaging.handleMessageQueue();
+    public static void run() {
+        while (true) {
+            try {
+                Debug.emptyIndicatorStrings();
+                Sensing.updateNearbyEnemies();
+                Messaging.handleMessageQueue();
 
-        // If there are more enemies than allies nearby, retreat to the
-        // nearest archon.
-        if (attackableTraitors.length + attackableZombies.length > veryCloseAllies.length) {
-          Movement.retreatToArchon();
+                // If there are more enemies than allies nearby, retreat to the
+                // nearest archon.
+                if (attackableTraitors.length + attackableZombies.length > veryCloseAllies.length) {
+                    Movement.retreatToArchon();
+                }
+
+                // Retreat if our health is less than 50%
+                if (rc.getHealth() < myRobotType.maxHealth / 2) {
+                    Movement.retreatToArchon();
+                    rc.setIndicatorString(2, "Retreating to archon @: " + nearestArchon.toString());
+                }
+
+                // This is redundant checking...
+                if (attackableTraitors.length > 0) {
+                    Offensive.attack(enemyTeam);
+                    rc.setIndicatorString(0, "Attacking Traitors");
+                } else if (attackableZombies.length > 0) {
+                    Offensive.attack(Team.ZOMBIE);
+                    rc.setIndicatorString(0, "Attacking Zombies");
+                } else {
+                    Movement.moveToClosestEnemy();
+                    rc.setIndicatorString(0, "Moving");
+                }
+
+                Clock.yield();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                e.printStackTrace();
+            }
         }
-
-        // Retreat if our health is less than 50%
-        if (rc.getHealth() < myRobotType.maxHealth / 2) {
-          Movement.retreatToArchon();
-          rc.setIndicatorString(2, "Retreating to archon @: " + nearestArchon.toString());
-        }
-
-        // This is redundant checking...
-        if (attackableTraitors.length > 0) {
-          Offensive.attack(enemyTeam);
-          rc.setIndicatorString(0, "Attacking Traitors");
-        } else if (attackableZombies.length > 0) {
-          Offensive.attack(Team.ZOMBIE);
-          rc.setIndicatorString(0, "Attacking Zombies");
-        } else {
-          Movement.moveToClosestEnemy();
-          rc.setIndicatorString(0, "Moving");
-        }
-
-        Clock.yield();
-      } catch (Exception e) {
-        System.out.println(e.getMessage());
-        e.printStackTrace();
-      }
     }
-  }
 }
--- src/shazbot/ViperClass.java
+++ src/shazbot/ViperClass.java
@@ -4,15 +4,15 @@ import battlecode.common.Clock;
 
 public class ViperClass extends RobotPlayer {
 
-  public static void move() {
+    public static void move() {
 
-  }
+    }
 
-  public static void attack() {
+    public static void attack() {
 
-  }
+    }
 
-  public static void run() {
+    public static void run() {
 
-  }
+    }
 }
