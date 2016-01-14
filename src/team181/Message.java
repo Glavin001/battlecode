d76b7db08a42ccec21f9a98ead7420234d02d56c
Glavin Wiechert <glavin.wiechert@gmail.com>
2016-01-14 12:00:20 -0400

    Improve Scouts, Archon, and general Robot messaging
    Allow general Robots to process map bounds and zombie dens.

--- src/team181/Message.java
+++ src/team181/Message.java
@@ -11,29 +11,46 @@ public class Message {
     private int part1 = 0;
     private int part2 = 0;
     
-    // Offset that shifts tags and ids past the last 16 bits of the message. 
+    /**
+     *  Offset that shifts tags and ids past the last 16 bits of the message. 
+     */
     private int offset = (int)Math.pow(2.0, 16);
     
-    //Construct message from incoming signal
+    /** 
+     * Construct message from incoming signal
+     * @param signal
+     */
     public Message(Signal signal){
         part1 = signal.getMessage()[0];
         part2 = signal.getMessage()[1];
     }
     
-    //Construct a new message from scratch
+    /** 
+     * Construct a new message from scratch
+     * @param tag
+     * @param loc
+     * @param id
+     */
     public Message(int tag, MapLocation loc, int id){
         setTag(tag);
         setLocation(loc);
         setID(id);
     }
     
-    //Construct a new message from scratch without an id
+    /** 
+     * Construct a new message from scratch without an id
+     * @param tag
+     * @param loc
+     */
     public Message(int tag, MapLocation loc){
         setTag(tag);
         setLocation(loc);
     }    
     
-    //Construct a new message from scratch without an id or location
+    /** 
+     * Construct a new message from scratch without an id or location
+     * @param tag
+     */
     public Message(int tag){
         setTag(tag);
     }       
@@ -72,7 +89,12 @@ public class Message {
         return loc;
     }
      
-    // Don't use this!!! use Messaging.sendMessage() instead. 
+    /**
+     * Don't use this!!! use Messaging.sendMessage() instead. 
+     * @param rc
+     * @param radiusSquared
+     * @throws GameActionException
+     */
     public void send(RobotController rc, int radiusSquared) throws GameActionException{
         rc.broadcastMessageSignal(part1, part2, radiusSquared);
     }
