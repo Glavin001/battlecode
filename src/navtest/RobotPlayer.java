bb13331f17c7cbb94f469e3c432ada9d3c0f0555
unknown <s1373037@MN122-03.SMUNET.SMU.CA>
2016-01-21 14:35:19 -0400

    Updated bugnav

--- src/navtest/RobotPlayer.java
+++ src/navtest/RobotPlayer.java
@@ -1,5 +1,7 @@
 package navtest;
 
+import java.util.Random;
+
 import battlecode.common.*;
 
 public class RobotPlayer {
@@ -8,7 +10,9 @@ public class RobotPlayer {
     public static boolean isBugging = false;
     public static int leastDistToTarget = 0;
     public static MapLocation[] previousLocations = {new MapLocation(0,0), new MapLocation(0,0)};
-    
+    public static MapLocation myCurrentLocation;
+    public static boolean firstBug = false;
+
     public static boolean directMove(Direction dir) throws GameActionException{
         if(rc.canMove(dir)){
             updatePreviousLocations();
@@ -25,29 +29,70 @@ public class RobotPlayer {
     
     public static void updatePreviousLocations(){
         previousLocations[1] = previousLocations[0];
-        previousLocations[0] = rc.getLocation();
+        previousLocations[0] = myCurrentLocation;
     }
     
-    public static void bugNav(MapLocation target) throws GameActionException{
-        Direction dirToTarget = rc.getLocation().directionTo(target);
+    // Better than directionTo(); returns diagonals
+    public static Direction getDirToTarget(MapLocation start, MapLocation target){
+        if(start.x > target.x){
+            if(start.y > target.y){
+                return Direction.NORTH_WEST;
+            }else if(start.y < target.y){
+                return Direction.SOUTH_WEST;
+            }else{
+                return Direction.WEST;
+            }
+        }else if(start.x < target.x){
+            if(start.y > target.y){
+                return Direction.NORTH_EAST;
+            }else if(start.y < target.y){
+                return Direction.SOUTH_EAST;
+            }else{
+                return Direction.EAST;
+            }
+        }else{
+            if(start.y > target.y){
+                return Direction.NORTH;
+            }else{
+                return Direction.SOUTH;
+            }
+        }
+    }
+    
+    public static boolean bugNav(MapLocation target) throws GameActionException{
+        //Don't bother to move if we are already there!
+        if(target.equals(myCurrentLocation)){
+            return true;
+        }
+        Direction dirToTarget = getDirToTarget(myCurrentLocation, target);
+        rc.setIndicatorString(2, "Target direction is: " + dirToTarget.toString());
         // If we were not able to move, start bugging or just move directly
         if(!isBugging){
             if(!directMove(dirToTarget)){
                 isBugging = true;
+                firstBug = true;
                 //Save where we last were
                 leastDistToTarget = getDistToTarget(target);
-                rc.setIndicatorString(1, "My previous least dist location was at: " + rc.getLocation().toString());
+                rc.setIndicatorString(1, "My previous least dist location was at: " + myCurrentLocation.toString());
             }            
         }
         // Need to check for bugging again if we want to move on turn we hit wall after direct move 
+        rc.setIndicatorString(0, "Am I bugging? " + Boolean.toString(isBugging));
         if(isBugging){
             boolean canMove = false;
-            Direction nextDirection = dirToTarget.rotateRight();
+            //Next direction depends upon whether this is our first bug move or not
+            Direction nextDirection;
+            if(firstBug){
+                nextDirection = getDirToTarget(myCurrentLocation, target).rotateRight();                                
+            }else{
+                nextDirection = getDirToTarget(myCurrentLocation, previousLocations[0]).rotateRight();                                
+            }
             int directionsTried = 0;
             // Break when we move, don't get stuck in infinite loop
             while(true){
-                MapLocation nextLocation = rc.getLocation().add(nextDirection);
+                MapLocation nextLocation = myCurrentLocation.add(nextDirection);
                 // Don't move to previous locations
+                rc.setIndicatorDot(nextLocation, 255, 60, 60);
                 if(rc.canMove(nextDirection)){ 
                         if(nextLocation.equals(previousLocations[0]) || nextLocation.equals(previousLocations[1])){
                             if(directionsTried < 7){
@@ -63,14 +108,15 @@ public class RobotPlayer {
                 }
                 
                 if(canMove){
-                    System.out.println("My two previous locations were: " 
-                            + previousLocations[0].toString()
-                            + previousLocations[1].toString()
-                            + " Going to: " + nextLocation.toString());          
+//                    System.out.println("My two previous locations were: " 
+//                            + previousLocations[0].toString()
+//                            + previousLocations[1].toString()
+//                            + " Going to: " + nextLocation.toString());          
                     updatePreviousLocations();
                     rc.move(nextDirection);
+                    firstBug = false;
                     // If we get closer to the target, then stop bugging
-                    if(getDistToTarget(target) < leastDistToTarget){
+                    if(getDistToTarget(target) <= leastDistToTarget){
                         isBugging = false;
                     }     
                     break;
@@ -80,21 +126,30 @@ public class RobotPlayer {
                 }
             }       
         }
+        return false;
     }
     
     public static void run(RobotController inrc){
         
         rc = inrc;
         MapLocation target = new MapLocation(16,4);
-        previousLocations[0] = rc.getLocation();
-        previousLocations[1] = rc.getLocation();
+        myCurrentLocation = rc.getLocation();
+        previousLocations[0] = myCurrentLocation;
+        previousLocations[1] = myCurrentLocation;
+        boolean atTarget = false;
+        Random rand = new Random(rc.getID());
         while(true){
             try{
-                rc.setIndicatorString(0, "Am I bugging? " + Boolean.toString(isBugging));
+                myCurrentLocation = rc.getLocation();
                 rc.setIndicatorDot(previousLocations[0], 80, 255, 80);
                 rc.setIndicatorDot(previousLocations[1], 80, 255, 80);
+                rc.setIndicatorDot(target, 200, 60, 200);
+//                rc.setIndicatorString(1, "My target is at: " + target.toString());
                 if(rc.isCoreReady()){
-                    bugNav(target);
+                    atTarget = bugNav(target);
+                }
+                if(atTarget){
+                    target = new MapLocation(rand.nextInt(30), rand.nextInt(30));
                 }
                 Clock.yield();
             }catch(GameActionException e){
